fdc407fca1f55071c6c70394f7c6ec9653860fd5
soma9k <1911140006x@ed.fuk.kindai.ac.jp>
2021-10-22 06:24:27 +0000

    first commit

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,17 +6,17 @@ class TopController < ApplicationController
   def login
     user = User.find_by(uid: params[:uid])
     if user and BCrypt::Password.new(user.pass) == params[:pass]
-      #TODO: ログイン成功したことをユーザに知らせる
+      flash[:notice] = 'ログインしました'
       session[:login_uid] = user.uid
       redirect_to root_path
     else
-      #TODO: エラーメッセージ
+      flash[:notice] = 'ログインエラー'
       render 'login'
     end
   end
 
   def logout
-    #TODO: ログアウトに成功したことをユーザに知らせる
+   flash[:notice] = 'ログアウトしました'
     session.delete(:login_uid)
     redirect_to root_path
   end
